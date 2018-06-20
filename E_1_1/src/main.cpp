38457235ec929ff4c6594f107aea45a9b97258bb
Kristofer Björnson <dafer45@hotmail.com>
2018-06-20 10:00:13 +0200

    Began work on first exercise.

---
+++ E_1_1/src/main.cpp
@@ -0,0 +1,40 @@
+/* Copyright 2016 Kristofer Björnson
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/** @package TBTKtemp
+ *  @file main.cpp
+ *  @brief New project
+ *
+ *  Empty template project.
+ *
+ *  @author Kristofer Björnson
+ */
+
+#include "TBTK/Streams.h"
+
+#include <complex>
+
+using namespace std;
+using namespace TBTK;
+
+double LOWER_BOUND = -10;
+double UPPER_BOUND = 10;
+
+int main(int argc, char **argv){
+//	Property::DOS dos(LOWER_BOUND, UPPER_BOUND, RESOLUTION);
+//	double N = calculateN(dos);
+
+	return 0;
+}
